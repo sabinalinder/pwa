513641cc5b00f90990fc475c6121e88ba3f65634
Sabina Linder <sabina.linder@aller.com>
2018-12-04 15:55:03 +0100

    Fixed more PWA details

--- src/worker.js
+++ src/worker.js
@@ -1,54 +1,82 @@
-// Set to true in production
-var doCache = false;
+let version = 'v1::';
 
-var CACHE_NAME = 'pwa-app-cache';
-
-// Delete old cache
-self.addEventListener('activate', event => {
-    const currentCachelist = [CACHE_NAME];
+self.addEventListener('install', function(event) {
+    console.log('WORKER: Installing...');
     event.waitUntil(
-        caches.keys()
-            .then(keyList =>
-                Promise.all(keyList.map(key => {
-                    if (!currentCachelist.includes(key)) {
-                        return caches.delete(key);
-                    }
-                }))
-            )
+        caches.open(version + 'fundamentals')
+        .then(function(cache) {
+            return cache.addAll([
+                '/'
+                // Add more urls
+            ]);
+        })
+        .then(function() {
+            console.log('WORKER: Install completed.')
+        })
     );
 });
 
-// Triggers when user starts the app
-self.addEventListener('install', function(event) {
-    if (doCache) {
-        event.waitUntil(
-            caches.open(CACHE_NAME)
-                .then(function(cache) {
-                    fetch('asset-manifest.json')
-                        .then(response => {
-                            response.json();
-                        })
-                        .then(assets => {
-                            // Cache initial page and main.js
-                            // Images, css etc can also be cached here
-                            const urlsToCache = [
-                                '/',
-                                assets['main.js']
-                            ];
-                            cache.addAll(urlsToCache);
-                        })
-                })
-        );
-    }
-});
-
-// Intercept request and serve matching files
 self.addEventListener('fetch', function(event) {
-    if(doCache) {
-        event.respondWith(
-            caches.match(event.request).then(function(response) {
-                return response || fetch(event.request);
-            })
-        );
+    console.log('WORKER: Fetching...');
+    if (event.request.method !== 'GET') {
+        console.log('WORKER: Fetch event ignored. ', event.request.method, event.request.url);
+        return;
     }
-});
+    event.respondWith(
+        cached.match(event.request)
+        .then(function(cached) {
+            var networked = fetch(event.request)
+            .then(fetchedFromNetwork, unableToResolve)
+            .catch(unableToResolve)
+            console.log('WORKER: Fetch event ', cached ? '(cached)' : '(network)', event.request.url);
+            return cached || networked;
+
+            function fetchedFromNetwork(response) {
+                var cacheCopy = response.clone();
+                console.log('WORKER: Fetch response from network.', event.request.url);
+
+                cached.open(version + 'pages')
+                .then(function add(cache) {
+                    cache.put(event.request, cacheCopy);
+                })
+                .then(function() {
+                    console.log('fetch response from cache.', event.request.url);
+                });
+                return response;
+            }
+
+            function unableToResolve() {
+                console.log('WORKER: Fetch request failed in both cache and network.');
+
+                return new Response('<h1>Service Unavailable</h1>', {
+                    status: 503,
+                    statusText: 'Service Unavailable',
+                    headers: new Headers({
+                        'Content-Type': 'text/html'
+                    })
+                });
+            }
+        })
+    )
+})
+
+self.addEventListener('activate', function(event) {
+    console.log('WORKER: Activating...');
+
+    event.waitUntil(
+        caches.keys()
+        .then(function(keys) {
+            return Promise.all(
+                keys.filter(function(key) {
+                    return !key.startsWith(version);
+                })
+                .map(function(key) {
+                    return caches.delete(key);
+                })
+            );
+        })
+        .then(function() {
+            console.log('WORKER: Activate completed.');
+        })
+    );
+})
